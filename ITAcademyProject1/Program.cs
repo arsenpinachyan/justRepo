b576f1ae22927ed827eef970f986a0ccf41d5c1d
Arsen <arsenep@inbox.ru>
2021-11-11 23:42:25 +0400

    Initial commit

---
+++ ITAcademyProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITAcademyProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
